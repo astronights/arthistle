3bf009f9e429479c7c76ad7823ec89f37ced4a64
astronights <shubhankar.a31@gmail.com>
2022-06-26 19:19:09 +0800

    Minor change

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
 
 // Apply routes before error handling
-app.use("/", router);
 app.use(express.static(path.resolve(__dirname, "../ui/build")));
+app.use("/", router);
 
 export default app;
