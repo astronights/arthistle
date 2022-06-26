e1d5246decd66da2cd8723834bcb548c58857666
astronights <shubhankar.a31@gmail.com>
2022-06-26 19:30:01 +0800

    More changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,9 +6,9 @@ import path from "path";
 const app = express();
 
 // Apply most middleware first
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "*" }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors({ origin: "*" }));
 
 // Apply routes before error handling
 app.use(express.static(path.resolve(__dirname, "../ui/build")));
