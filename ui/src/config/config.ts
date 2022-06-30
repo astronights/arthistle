9cff35041ea53d10df269b819a90e626ff2de3ab
astronights <shubhankar.a31@gmail.com>
2022-06-30 14:16:08 +0800

    Some temp change

--- ui/src/config/config.ts
+++ ui/src/config/config.ts
@@ -1,7 +1,7 @@
 import pack from "../../package.json";
 
 const config = {
-  server: `http://localhost:${process.env["PORT"]}`,
+  server: `http://localhost:${process.env.PORT || process.env.REACT_APP_PORT}`,
   // server: pack.proxy,
 };
 export default config;
