4dae2d9323ce35065c7b6549bb3c80453cd6d9bd
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2025-03-01 23:41:11 +0800

    Updating server

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,10 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
 app.use("/", router);
 
-app.use(express.static(path.join(__dirname + "../../../../ui/build")));
+// app.use(express.static(path.join(__dirname + "../../../../ui/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../../../../ui/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "../public/index.html"));
+// });
 
 export default app;
