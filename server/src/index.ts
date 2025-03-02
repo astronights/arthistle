f6d17cf7185bfa30769e5fe178fcbade69e1b490
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2025-03-02 10:43:26 +0800

    Updating to index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,26 @@
-import app from './app';
+import cors from "cors";
+import express from "express";
+import RateLimit from "express-rate-limit";
+
+import router from "./api/router";
 import config from './config/config';
 
-module.exports = app;
+const limiter = RateLimit({
+  windowMs: 60 * 1000,
+  max: 100,
+});
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(config.port, () => {
-    console.log(`Started server at ${config.host}:${config.port}`);
-  });
-}
+const app = express();
+
+app.use(limiter);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
+app.use("/", router);
+
+app.listen(config.port, () => {
+  console.log(`Started server at ${config.host}:${config.port}`);
+});
+
+module.exports = app;
