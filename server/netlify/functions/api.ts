a0ec46b66444f42a4ee4499906371c9e7916c5af
Shubhankar Agrawal <e0925482@u.nus.edu>
2025-03-06 20:51:03 +0800

    Update api.ts

--- server/netlify/functions/api.ts
+++ server/netlify/functions/api.ts
@@ -1,19 +1,12 @@
 import cors from "cors";
 import express from "express";
 import serverless from "serverless-http";
-import RateLimit from "express-rate-limit";
 
 import router from "../../src/api/router";
 import config from '../../src/config/config';
 
-const limiter = RateLimit({
-  windowMs: 60 * 1000,
-  max: 100,
-});
-
 const app = express();
 
-app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
