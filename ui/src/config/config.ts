f5876de8670b196002d1519b52289329dc21a4ee
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2025-03-10 20:32:16 +0800

    Fix inception date

--- ui/src/config/config.ts
+++ ui/src/config/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  inception: new Date("July 1, 2024 00:00:00"),
+  inception: new Date("March 1, 2025 00:00:00"),
   server: import.meta.env.VITE_APP_SERVER,
 };
 export default config;
